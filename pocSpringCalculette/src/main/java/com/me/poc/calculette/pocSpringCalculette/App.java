6ea17919bfbce7099ce82043f81a29cf22e177e5
a.olivier <adrien.olivier4@gmail.com>
2015-05-05 22:56:48 +0200

    creation du squelette de l'application

---
+++ pocSpringCalculette/src/main/java/com/me/poc/calculette/pocSpringCalculette/App.java
@@ -0,0 +1,13 @@
+package com.me.poc.calculette.pocSpringCalculette;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
